48af908876221dd0a47fbeebf4e559075d0da97b
Sergei Kompanietc <delightvlg@gmail.com>
2021-07-25 14:39:02 +0300

    heroku deploy

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
-const { MONGO_URL = 'mongodb://localhost:27017/bitfilmsdb' } = process.env;
+const MONGO_URL = process.env.MONGODB_URI;
 
 module.exports = {
   PORT,
