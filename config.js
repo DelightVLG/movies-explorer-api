c0c89b4a30b7b38eaf2d45d36747d524a91e5fab
Sergei Kompanietc <delightvlg@gmail.com>
2021-07-25 15:15:24 +0300

    fix: mongoDB path test

--- config.js
+++ config.js
@@ -1,7 +1,8 @@
 require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
-const MONGO_URL = process.env;
+// const MONGO_URL = process.env;
+const MONGO_URL = 'mongodb+srv://delightvlg:Zpw8uOf88G9MqzSA@movies-explorer-db.d3pxa.mongodb.net/movies-explorer-db?retryWrites=true&w=majority';
 
 module.exports = {
   PORT,
