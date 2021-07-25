fde034af664b54280650ed196c0dfbcbb9c6cfdf
Sergei Kompanietc <delightvlg@gmail.com>
2021-07-25 14:46:01 +0300

    fix: added Mongo URI to .env

--- config.js
+++ config.js
@@ -1,12 +1,9 @@
 require('dotenv').config();
 
 const { PORT = 3000 } = process.env;
-const MONGO_URL = process.env.MONGODB_URI;
+const MONGO_URL = process.env;
 
 module.exports = {
   PORT,
   MONGO_URL,
 };
-
-// domains api.vlmovies.students.nomoredomains.rocks www.api.vlmovies.students.nomoredomains.rocks
-// vlmovies.students.nomoredomains.rocks
